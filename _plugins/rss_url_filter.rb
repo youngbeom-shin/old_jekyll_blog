b5a42a459627c96bb4b55200ab7ef22e03cdccf7
shen yongfan <474040792@qq.com>
2018-07-10 18:58:44 +0800

    commit

--- _plugins/rss_url_filter.rb
+++ _plugins/rss_url_filter.rb
@@ -1,7 +1,7 @@
 module Jekyll
   module RSSURLFilter
     def relative_urls_to_absolute(input)
-      url = Jekyll.configuration({})['url'] || 'https://blog.youngbeom.com/'
+      url = Jekyll.configuration({})['url'] || 'http://note.rpsh.net'
       input.gsub('src="/assets/', 'src="' + url + '/assets/').gsub('href="/assets/', 'href="' + url + '/assets/')
     end
   end
